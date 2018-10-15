7f9e45a2dee16bdae4f92ab514e983cf3d767617
Harman Dhillon <harmandhi22@gmail.com>
2018-10-15 12:54:12 -0400

    init commit

---
+++ spellingChecker/cmake-build-debug/CMakeFiles/spellingChecker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spellingChecker.dir/AvlTest.cpp.obj"
+  "spellingChecker.pdb"
+  "spellingChecker.exe"
+  "spellingChecker.exe.manifest"
+  "spellingChecker.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spellingChecker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
