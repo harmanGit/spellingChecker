a815a79d105618eda125c8f7ade2411dac33c42b
Harman Dhillon <harmandhi22@gmail.com>
2018-10-17 14:35:20 -0400

    parsing issue fixed, it was the newline

--- harmanDhillonHW2/harmanDhillonHW2/AvlTest.cpp
+++ harmanDhillonHW2/harmanDhillonHW2/AvlTest.cpp
@@ -17,37 +17,35 @@ int main()
 	string fileName;
 	string textFileName;
 	string dictionaryFileName;
+	ifstream textFile;
+	ifstream dictionaryFile;
 	string singleLine;
 	string singleWord;
 	char singleChar;
 
 	cout << "Please enter the name for the file you want to spell check ---> " << endl;
 	cin >> fileName;
-
 	textFileName = fileName + ".txt";
+	cout << "Please enter the name for of the dictionary you want to use to check ---> " << endl;
+	cin >> fileName;
 	dictionaryFileName = fileName +  ".dct";
 
-	cout << textFileName << " || " << dictionaryFileName << endl;
-
-	ifstream textFile;
-	ifstream dictionaryFile;
 	textFile.open(textFileName);
 	dictionaryFile.open(dictionaryFileName);
 	if (textFile.is_open())
 	{
-		while (getline(textFile, singleLine))
+		while (getline(textFile, singleLine))//going line by line in the text file
 		{
-			for (int i = 0; i < singleLine.length(); ++i)
+			singleLine += "#";// used to detect newlines
+			for (int i = 0; i < singleLine.length(); ++i)//going char by char in the single line
 			{
-				singleChar = tolower(singleLine[i]);
-				if (isalpha(singleChar))
-					singleWord += singleChar;
+				singleChar = tolower(singleLine[i]);// converting to lowercase if char is uppercase
+				if (isalpha(singleChar))//checking if char thats lowercase is a letter
+					singleWord += singleChar;//adding it too the word
 				else
 				{
-					cout << singleWord << endl;
-					cout << "||";
-					//words.insert(singleWord);
-					singleWord = "";
+					words.insert(singleWord);//inserting the word to the avl tree
+					singleWord = "";//resetting the word
 				}	
 			}		
 		}
@@ -56,25 +54,25 @@ int main()
 	else cout << "File error, when attempting to open the given text file." << endl;
 
 
-	//if (dictionaryFile.is_open())
-	//{
-	//	while (getline(dictionaryFile, singleWord))
-	//	{
-	//		if (words.find(singleWord) != ITEM_NOT_FOUND)
-	//		{
-	//			words.remove(singleWord);
-	//		}
-	//	}
-	//	dictionaryFile.close();
-	//}
-	//else cout << "File error, when attempting to open the given dictionary file." << endl;
+	if (dictionaryFile.is_open())
+	{
+		while (getline(dictionaryFile, singleWord))
+		{
+			if (words.find(singleWord) != ITEM_NOT_FOUND)
+			{
+				words.remove(singleWord);
+			}
+		}
+		dictionaryFile.close();
+	}
+	else cout << "File error, when attempting to open the given dictionary file." << endl;
 
-	//cout << "SIZE: " << words.getAVLTreeSize();
+	cout << "SIZE: " << words.getAVLTreeSize();
 
-	//ofstream fileWriter("errorFile.txt");
-	//fileWriter << "Error File" << endl;
+	ofstream fileWriter("errorFile.txt");
+	fileWriter << "Error File" << endl;
 
-	//words.printTree();
+	words.printTree();
 
 	char c;
 	cin >> c;
