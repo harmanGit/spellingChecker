6bda603f3a482f2f59fae51aacb076bdfafdd9a6
Harman Dhillon <harmandhi22@gmail.com>
2018-10-18 23:55:16 -0400

    90% completed

--- harmanDhillonHW2/harmanDhillonHW2/AvlTest.cpp
+++ harmanDhillonHW2/harmanDhillonHW2/AvlTest.cpp
@@ -5,15 +5,72 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <map>
+#include <queue>
 #include "AvlTree.h"
 using namespace std;
 
 const string ITEM_NOT_FOUND = "99999";
-ofstream fileWriter;
 
-void writeErrorFile(string &s)
+class word
 {
-	fileWriter << s << endl;
+private:
+	string aWord;
+	queue<int> line;
+	int numberOfOccurrences;
+public:
+	word() { aWord = ""; numberOfOccurrences = 0; queue<int> empty; swap(line, empty);};
+
+	word(string w, int l, int n)
+	{
+		aWord = w;
+		line.push(l);
+		numberOfOccurrences = n;
+	}
+	void setWord(string newWord) { aWord = newWord; }
+	void addLine(int lineValue) { line.push(lineValue); }
+	void incrementNumberOfOccurrences() { ++numberOfOccurrences; }
+
+	string getWord() { return aWord; }
+	string getLine() 
+	{
+		string returnString = "";
+		while (!line.empty())
+		{
+			returnString += ", " + to_string(line.front());
+			line.pop();
+		}
+		return returnString;
+	}
+
+	int getNumberOfOccurrences() { return numberOfOccurrences; }
+	string getPageNumber()
+	{
+		if (line.size() % 66 == 0)
+			return to_string(66);
+		return to_string(line.size() / 66 + 1);
+	}
+};
+
+map<string, word> allWords;
+ofstream fileWriter;
+void writeErrorFile(string &str)
+{
+	fileWriter << "Incorrect Spelled Word: " << str << endl;
+	fileWriter << "Number Of Occurrences: " << allWords[str].getNumberOfOccurrences() << endl;
+	fileWriter << "Page Numbers: " << allWords[str].getPageNumber() << endl;
+	fileWriter << "Lines at error found: " << allWords[str].getLine() << endl;//print list
+
+}
+
+string toLower(string s)
+{
+	string returnString = "";
+	for (int i = 0; i < s.length(); i++)
+	{
+		returnString += tolower(s[i]);
+	}
+	return returnString;
 }
 
 int main()
@@ -27,6 +84,7 @@ int main()
 	ifstream dictionaryFile;
 	string singleLine;
 	string singleWord;
+	int lineCount;
 	char singleChar;
 
 	cout << "Please enter the name for the file you want to spell check ---> ";
@@ -40,8 +98,10 @@ int main()
 	dictionaryFile.open(dictionaryFileName);
 	if (textFile.is_open())
 	{
+		lineCount = 0;
 		while (getline(textFile, singleLine))//going line by line in the text file
 		{
+			++lineCount;
 			singleLine += "#";//used to detect new lines
 			for (int i = 0; i < singleLine.length(); ++i)//going char by char in the single line
 			{
@@ -51,7 +111,22 @@ int main()
 				else if (singleWord.length() >= 1)
 				{
 					words.insert(singleWord);//inserting the word to the avl tree
-					singleWord = "";//resetting the word
+
+					map<string, word>::iterator itr;
+					itr = allWords.find(singleWord);
+					if (itr != allWords.end())
+					{
+						itr->second.incrementNumberOfOccurrences();//existing word, count++
+						itr->second.addLine(lineCount);//adding a new line to the existing word object
+					}
+					else
+					{
+						word wordData(singleWord, lineCount, 0);
+						wordData.incrementNumberOfOccurrences();
+						allWords[singleWord] = wordData;//new word
+					}
+						singleWord = "";//resetting the word
+
 				}
 			}
 		}
@@ -64,15 +139,15 @@ int main()
 	if (dictionaryFile.is_open())
 	{
 		while (getline(dictionaryFile, singleWord))
-			if (words.find(singleWord) != ITEM_NOT_FOUND)
-				words.remove(singleWord);
+			if (words.find(toLower(singleWord)) != ITEM_NOT_FOUND)
+				words.remove(toLower(singleWord));
 		dictionaryFile.close();
 	}
 	else cout << "File error, when attempting to open the given dictionary file." << endl;
 
 	cout << "SIZE: " << words.getAVLTreeSize() << endl;
 
-	words.printTree();
+	//words.printTree();
 
 	words.traverse(writeErrorFile);
 
